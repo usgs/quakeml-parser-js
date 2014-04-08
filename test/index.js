03445395c485066acc5a47cf1cd25cb9bd837792
Jeremy Fee <jmfee@usgs.gov>
2014-04-08 08:52:58 -0600

    Add QuakemlTest to test harness

--- test/index.js
+++ test/index.js
@@ -1,8 +1,22 @@
+'use strict';
+
+if (typeof Array.prototype.forEach === 'undefined') {
+	// for phantomjs
+	Array.prototype.forEach = function (callback) {
+		var i, len;
+		for (i = 0, len = this.length; i < len; i++) {
+			callback(this[i], i, this);
+		}
+	};
+}
+
+
 require.config({
 	baseUrl: '..',
 	paths: {
 		mocha: 'mocha/mocha',
-		chai: 'chai/chai'
+		chai: 'chai/chai',
+		Quakeml: 'quakeml/Quakeml'
 	},
 	shim: {
 		mocha: {
@@ -12,7 +26,13 @@ require.config({
 			deps: ['mocha'],
 			exports: 'chai'
 		}
-	}
+	},
+	packages: [
+		{
+			name: 'quakeml',
+			main: 'Quakeml'
+		}
+	]
 });
 
 require([
@@ -20,12 +40,12 @@ require([
 ], function (
 	mocha
 ) {
-	'use strict';
 
 	mocha.setup('bdd');
 
 	// Add each test class here as they are implemented
 	require([
+		'spec/QuakemlTest'
 	], function () {
 		if (window.mochaPhantomJS) {
 			window.mochaPhantomJS.run();
