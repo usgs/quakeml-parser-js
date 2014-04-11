04b0d5048b7e1929cebdcf8968671157f8b24e19
Jeremy Fee <jmfee@usgs.gov>
2014-04-11 17:29:37 -0600

    Added XmlUtilTest

---
+++ test/spec/XmlUtilTest.js
@@ -0,0 +1,43 @@
+/* global define, describe, it */
+define([
+	'chai',
+
+	'quakeml/XmlUtil'
+], function (
+	chai,
+
+	XmlUtil
+) {
+	'use strict';
+	var expect = chai.expect;
+
+	describe('XmlUtil unit tests', function () {
+
+		describe('xmlToJson', function () {
+
+			it('parses xml strings', function () {
+				var xmlString = '<el><child1>value1</child1></el>',
+				    json = XmlUtil.xmlToJson(xmlString);
+
+				expect(json.el.child1).to.equal('value1');
+			});
+
+			it('uses array if multiple elements have the same name', function () {
+				var xmlString,
+				    json;
+
+				xmlString = '<el>' +
+						'<child1>value1</child1>' +
+						'<child1>value2</child1>' +
+						'</el>';
+				json = XmlUtil.xmlToJson(xmlString);
+				expect(Array.isArray(json.el.child1)).to.equal(true);
+				expect(json.el.child1[0]).to.equal('value1');
+				expect(json.el.child1[1]).to.equal('value2');
+			});
+
+		});
+
+	});
+
+});
